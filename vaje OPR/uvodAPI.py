22b137fc4c02ff0ecdf4e50c1baf50626a6ee772
caremare1 <jstnajbolj@gmail.com>
2026-09-14 06:10:59 +0000

    commit

--- vaje OPR/uvodAPI.py
+++ vaje OPR/uvodAPI.py
@@ -8,15 +8,15 @@
 # print(slovar["ključ2"])
 
 # raznoliki slovar
-razno = {"stevilo" : 6,
-         "ime" : "Marko",
-         "seznam" : [1,2,3,4],
-         "slovar" : {"firma" : "Audi", "moč" : "120kW"}}
-print(razno["stevilo"] + 10)
-print(max(razno["seznam"]))
-print(razno["slovar"]) # {"firma" : "Audi", "moč" : "120kW"}
-print(razno["slovar"]["firma"])
-print(razno["slovar"]["moč"])
+# razno = {"stevilo" : 6,
+         # "ime" : "Marko",
+         # "seznam" : [1,2,3,4],
+         # "slovar" : {"firma" : "Audi", "moč" : "120kW"}}
+# print(razno["stevilo"] + 10)
+# print(max(razno["seznam"]))
+# print(razno["slovar"]) # {"firma" : "Audi", "moč" : "120kW"}
+# print(razno["slovar"]["firma"])
+# print(razno["slovar"]["moč"])
 
 # Open Meteo API
 import requests
@@ -24,3 +24,4 @@ base_url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.4
 
 call = requests.get(base_url).json()
 print(call["daily"]["rain_sum"][2])
+print(call["daily"]["time"])
